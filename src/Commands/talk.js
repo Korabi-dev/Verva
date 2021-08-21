5816378aab5b97241f0e20b71ee4a24ab80bf0ae
Korab Arifi <69105206+Korabi-dev@users.noreply.github.com>
2021-08-21 12:46:04 +0200

    Added cool new chat bot for fun

---
+++ src/Commands/talk.js
@@ -0,0 +1,23 @@
+const discord = require("discord.js")
+const { default: axios } = require("axios")
+module.exports = {
+    name: "talk",
+    /**
+     * 
+     * @param {discord.Client} client 
+     * @param {discord.Message} message 
+     * @param {String[]} args 
+     */
+    run: async(client, message, args) => {
+    if(!args[0]) return message.reply({embeds: [client.functions.error("You need to provide a message")]})
+        var reply = {message: "beep boop beep boop *robot go stupid*"}
+        try{
+            const { data } = await axios.get(client.config.chatbot_api + `?message=${args.all}`)
+            reply = data
+        }catch(e){
+            reply = {message: "beep boop beep boop *robot go stupid*"}
+        }
+        
+ message.reply({embeds: [client.functions.embed(`${client.user.username}:`, reply.message) ]})
+    }
+}
